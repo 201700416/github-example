bb799ab1b39ae0323dfc6b2800a20312dc186acc
Mohammed Ali Jaafar <201700416@student.polytechnic.bh>
2020-11-14 16:40:23 +0300

    add print to the viewDidLoad

---
+++ github example/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  github example
+//
+//  Created by mobileProg on 11/14/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let message = "Hello Git!"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print(message)
+    }
+
+
+}
+
